272dec91bb3010a59ee765d0fe9a2a28297e6058
Leo <leonardogrosales@gmail.com>
2018-01-26 18:21:31 +0000

    Update data bootstrapper

--- src/Payments.Domain/DataBootstrapper.cs
+++ src/Payments.Domain/DataBootstrapper.cs
@@ -77,9 +77,69 @@ namespace Payments.Domain
                     },
                     new Payment
                     {
-                        Id = "id2",
+                        Id = "216d4da9-e59a-4cc6-8df3-3da6e7580b77",
                         Version = 0,
-                        OrganisationId = "orgId2"
+                        OrganisationId = "743d5b63-8e6f-432e-a8fa-c5d8d2ee5fcb",
+                        Attributes = new PaymentAttributes
+                        {
+                            Amount = "100.21",
+                            BeneficiaryParty = new Party
+                            {
+                                AccountName = "R Lothbrok",
+                                AccountNumber = "31926819",
+                                AccountNumberCode = "BBAN",
+                                AccountType = 0,
+                                Address = "1 The Kattegat SE2",
+                                BankId = "403000",
+                                BankIdCode = "GBDSC",
+                                Name = "Ragnar Lothbrok"
+                            },
+                            ChargesInformation = new ChargesInformation
+                            {
+                                BearerCode = "SHAR",
+                                SenderCharges = new List<Charge>
+                                {
+                                    new Charge { Amount = "5.00", Currency = "GBP" },
+                                    new Charge { Amount = "10.00", Currency = "USD" }
+                                },
+                                ReceiverChargesAmount = "1.00",
+                                ReceiverChargesCurrency = "USD"
+                            },
+                            Currency = "GBP",
+                            DebtorParty = new Party
+                            {
+                                AccountName = "K Egbert",
+                                AccountNumber = "GB29XABC10161234567801",
+                                AccountNumberCode = "IBAN",
+                                Address = "10 Wessex Crescent Sourcetown NE1",
+                                BankId = "203301",
+                                BankIdCode = "GBDSC",
+                                Name = "King Egbert"
+                            },
+                            EndToEndReferecence = "For all the things I broke",
+                            Fx = new Fx
+                            {
+                                ContractReference = "FX123",
+                                ExchangeRate = "2.00000",
+                                OriginalAmount = "200.42",
+                                OriginalCurrency = "USD"
+                            },
+                            NumericReference = "1002001",
+                            PaymentId = "123456789012345678",
+                            PaymentPurpose = "Paying for goods/services",
+                            PaymentSheme = "FPS",
+                            PaymentType = "Credit",
+                            ProcessingDate = "2017-01-18",
+                            Reference = "Payment for the things Ragnar broke",
+                            SchemePaymentSubType = "InternetBanking",
+                            SchemePaymentType = "ImmediatePayment",
+                            SponsorParty = new Party
+                            {
+                                AccountNumber = "56781234",
+                                BankId = "123123",
+                                BankIdCode = "GBDSC"
+                            }
+                        }
                     }
                 };
         }
